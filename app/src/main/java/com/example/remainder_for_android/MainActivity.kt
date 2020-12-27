666f7352ee3f3eaacf84d3730ddd0c2df078bf13
a_yoshino <atsuko.yoshino@rakus.co.jp>
2020-12-28 00:38:42 +0900

    implement activity of button in regist remainder

--- app/src/main/java/com/example/remainder_for_android/MainActivity.kt
+++ app/src/main/java/com/example/remainder_for_android/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.remainder_for_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.widget.AdapterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun moveToRegist(view: View) {
+            val intent = Intent(applicationContext, RegistRemainderActivity::class.java)
+            startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/remainder_for_android/RegistRemainderActivity.kt
@@ -0,0 +1,53 @@
+package com.example.remainder_for_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Switch
+
+class RegistRemainderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regist_remainder)
+
+        // リスナクラスを設定
+        val btClear = findViewById<Button>(R.id.btClear)
+        val btCancel = findViewById<Button>(R.id.btCancel)
+        val btSubmit = findViewById<Button>(R.id.btSubmit)
+
+        val onClickInstance = createActionListner()
+        btSubmit.setOnClickListener(onClickInstance)
+        btClear.setOnClickListener(onClickInstance)
+        btCancel.setOnClickListener(onClickInstance)
+    }
+
+    private inner class createActionListner:View.OnClickListener {
+        override fun onClick(view:View) {
+            val contents = findViewById<EditText>(R.id.contents)
+            val date = findViewById<EditText>(R.id.date)
+            val time= findViewById<EditText>(R.id.time)
+            val tag = findViewById<Spinner>(R.id.tag_spinner)
+            val complete = findViewById<Switch>(R.id.complete)
+
+            when(view.id) {
+                R.id.btSubmit -> {
+                    finish()
+                }
+                R.id.btCancel -> {
+                    finish()
+                }
+                R.id.btClear -> {
+                    contents.setText("")
+                    date.setText("")
+                    time.setText("")
+                    tag.setSelection(0)
+                    complete.isChecked=false
+                }
+            }
+        }
+    }
+}
